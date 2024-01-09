8bdb86bf8dcfee75cade6752a960b6a29f8f7f6d
kamalbuilds <geniusamansingh@gmail.com>
2024-01-09 12:57:29 +0530

    deploy bug fixed +2

--- frontend/components/resumecomponents/TemplateRender/TemplateRender.jsx
+++ frontend/components/resumecomponents/TemplateRender/TemplateRender.jsx
@@ -115,6 +115,8 @@ function TemplateRenderer({
 
  const getVc = async () => {
 
+  if (!web5) return; // Optionally handle the case when web5 is false
+  try {
   const { records } = await web5.dwn.records.query({
     from: "did:ion:EiAWZrqejZkWXKpKG1XoiXc-kH5zENr8uZlPM7OaXYZSpQ:eyJkZWx0YSI6eyJwYXRjaGVzIjpbeyJhY3Rpb24iOiJyZXBsYWNlIiwiZG9jdW1lbnQiOnsicHVibGljS2V5cyI6W3siaWQiOiJkd24tc2lnIiwicHVibGljS2V5SndrIjp7ImNydiI6IkVkMjU1MTkiLCJrdHkiOiJPS1AiLCJ4IjoiOEtLNEpxWXJLWEoyeDRkMUZCT0RKVmdVNDJwRzZobVRRbWduRVAtTXpwNCJ9LCJwdXJwb3NlcyI6WyJhdXRoZW50aWNhdGlvbiJdLCJ0eXBlIjoiSnNvbldlYktleTIwMjAifSx7ImlkIjoiZHduLWVuYyIsInB1YmxpY0tleUp3ayI6eyJjcnYiOiJzZWNwMjU2azEiLCJrdHkiOiJFQyIsIngiOiJwV0w1VDZDTmFCNnF1T2NPMVpLZldvdHpJZTByWWJxOHRveTlwaUFzMXdnIiwieSI6ImxRaDBYUzl6Tm5yZHhuMjFha25oTG1TQzNCS0phLVhKM3g2WFlOb1djT0kifSwicHVycG9zZXMiOlsia2V5QWdyZWVtZW50Il0sInR5cGUiOiJKc29uV2ViS2V5MjAyMCJ9XSwic2VydmljZXMiOlt7ImlkIjoiZHduIiwic2VydmljZUVuZHBvaW50Ijp7ImVuY3J5cHRpb25LZXlzIjpbIiNkd24tZW5jIl0sIm5vZGVzIjpbImh0dHBzOi8vZHduLnRiZGRldi5vcmcvZHduMSIsImh0dHBzOi8vZHduLnRiZGRldi5vcmcvZHduMiJdLCJzaWduaW5nS2V5cyI6WyIjZHduLXNpZyJdfSwidHlwZSI6IkRlY2VudHJhbGl6ZWRXZWJOb2RlIn1dfX1dLCJ1cGRhdGVDb21taXRtZW50IjoiRWlEel83dndNUGExUEhsNmRvbWU5ZGk4OUZTc0Y2M2YtalVUOHdrVDkyQ1M3dyJ9LCJzdWZmaXhEYXRhIjp7ImRlbHRhSGFzaCI6IkVpQkk5Rm1KQndXT0RjbklrSmxCU0ZqQW9zTmV6c0NtVnU0QUdGUUJSbkxSdUEiLCJyZWNvdmVyeUNvbW1pdG1lbnQiOiJFaUE2SDJvS2RTZFVKM2h5OG5tSnZjWHozR3BmUmxkd1RJdlNVZnRnakJFbzVnIn19",
     message: {
@@ -124,6 +126,11 @@ function TemplateRenderer({
       },
     },
   });
+} catch (error) {
+  console.error("Error querying protocol:", error);
+  // Handle the error as needed
+  throw error; // Optionally rethrow the error if you want to propagate it
+}
 
   const vcs = records.map(async (record) => {
     const signedVcJwt = await record.data.text();
